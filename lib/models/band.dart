a5e7f50027fa37ee226cec7af064094485e1d62b
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-06-12 18:26:24 -0500

    Terminando La App De Graficas En Tiempo Real

--- lib/models/band.dart
+++ lib/models/band.dart
@@ -6,7 +6,10 @@ class Band {
   Band({required this.id, required this.name, required this.votes});
 
   /* El Factory Constructor Tiene Como Objetivo Regresar Una Nueva 
-    Instancia De Mi Clase */
-  factory Band.fromMap(Map<String, dynamic> obj) =>
-      Band(id: obj['id'], name: obj['name'], votes: obj['votes']);
+    Instancia De Mi Clase Mapeada Para Mas Facil Saber Que Tipos De Datos Tiene */
+  factory Band.fromMap(Map<String, dynamic> obj) => new Band(
+        id: obj.containsKey('id') ? obj['id'] : 'no-id',
+        name: obj.containsKey('name') ? obj['name'] : 'no-name',
+        votes: obj.containsKey('votes') ? obj['votes'] : 'no-votes',
+      );
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,10 @@
+import 'dart:ffi';
 import 'dart:io';
 import 'package:provider/provider.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 import 'package:band_names/services/socket.dart';
 
@@ -14,12 +16,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Band> bands = [
+  List<Band> bands = [];
+  /* [
     Band(id: '1', name: 'Metalica', votes: 5),
     Band(id: '2', name: 'Queen', votes: 1),
     Band(id: '3', name: 'Héroes Del Silencio', votes: 2),
     Band(id: '4', name: 'Bon Jovi', votes: 4),
-  ];
+  ]; */
+
+  /* Se Ejecuta Una Sola Vez */
+  @override
+  void initState() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+
+    /* Aqui Estamos Dejando La Referencia _handleActiveBands, Y Antes De
+    Que Se Construya El Componente Se Va A Ejecutar */
+    socketService.socket.on('active-bands', _handleActiveBands);
+    super.initState();
+  }
+
+  _handleActiveBands(dynamic payload) {
+    this.bands = (payload as List).map((band) => Band.fromMap(band)).toList();
+    setState(() {});
+  }
+
+  /* Para Cuando Se Destruya El Home, Para Que Deje De Escuchar Ese Evento */
+  @override
+  void dispose() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.socket.off('active-bands');
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,9 +73,16 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: bands.length,
-        itemBuilder: (context, index) => _bandTile(bands[index]),
+      body: Column(
+        children: [
+          if (bands.isNotEmpty) _showGraph(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: bands.length,
+              itemBuilder: (context, index) => _bandTile(bands[index]),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -59,14 +93,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _bandTile(Band band) {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+
     return Dismissible(
       key: Key(band.id),
       direction: DismissDirection.startToEnd,
-      onDismissed: (direction) {
-        print('direction: $direction');
-        print('id: ${band.id}');
-        //TODO: LLamar El Borrado En El Server
-      },
+      /* No Dejo Solo Una Referencia, Por Que Yo Necesito Que Se Construya Cuando Se De Click,
+      No Cuando Se Inicie Este Componente */
+      onDismissed: (_) => socketService.emit('delete-band', {'id': band.id}),
       background: Container(
         padding: EdgeInsets.only(left: 8),
         color: Colors.blue,
@@ -85,9 +119,7 @@ class _HomePageState extends State<HomePage> {
         ),
         title: Text(band.name),
         trailing: Text('${band.votes}', style: TextStyle(fontSize: 20)),
-        onTap: () {
-          print(band.name);
-        },
+        onTap: () => socketService.emit('vote-band', {'id': band.id}),
       ),
     );
   }
@@ -98,58 +130,100 @@ class _HomePageState extends State<HomePage> {
     if (!Platform.isAndroid) {
       return showCupertinoDialog(
         context: context,
-        builder: (context) {
-          return CupertinoAlertDialog(
-            title: Text('New Band Name: '),
-            content: CupertinoTextField(
-              controller: textController,
+        builder: (_) => CupertinoAlertDialog(
+          title: Text('New Band Name: '),
+          content: CupertinoTextField(
+            controller: textController,
+          ),
+          actions: [
+            CupertinoDialogAction(
+              child: Text('Add'),
+              isDefaultAction: true,
+              onPressed: () => addBandToList(textController.text),
             ),
-            actions: [
-              CupertinoDialogAction(
-                child: Text('Add'),
-                isDefaultAction: true,
-                onPressed: () => addBandToList(textController.text),
-              ),
-              CupertinoDialogAction(
-                child: Text('Dismiss'),
-                isDestructiveAction: true,
-                onPressed: () => Navigator.pop(context),
-              ),
-            ],
-          );
-        },
+            CupertinoDialogAction(
+              child: Text('Dismiss'),
+              isDestructiveAction: true,
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        ),
       );
     }
 
     showDialog(
       context: context,
-      builder: (_) {
-        return AlertDialog(
-          title: Text('New Band Name: '),
-          content: TextField(
-            controller: textController,
+      builder: (_) => AlertDialog(
+        title: Text('New Band Name: '),
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          MaterialButton(
+            child: Text('Add'),
+            elevation: 5,
+            textColor: Colors.blue,
+            onPressed: () => addBandToList(textController.text),
           ),
-          actions: [
-            MaterialButton(
-              child: Text('Add'),
-              elevation: 5,
-              textColor: Colors.blue,
-              onPressed: () => addBandToList(textController.text),
-            ),
-          ],
-        );
-      },
+        ],
+      ),
     );
   }
 
   void addBandToList(String bandName) {
     if (bandName.length > 1) {
-      this.bands.add(
-            new Band(id: DateTime.now().toString(), name: bandName, votes: 3),
-          );
-      setState(() {});
+      final socketService = Provider.of<SocketService>(context, listen: false);
+      socketService.emit('add-band', {'name': bandName});
     }
 
     Navigator.pop(context);
+  }
+
+  Widget _showGraph() {
+    Map<String, double> dataMap = {};
+
+    bands.forEach((band) {
+      dataMap.putIfAbsent(band.name, () => band.votes.toDouble());
+    });
+
+    /* final List<Color> colorList = [
+      Colors.blue[50]!,
+      Colors.blue[200]!,
+      Colors.pink[50]!,
+      Colors.pink[200]!,
+      Colors.yellow[50]!,
+      Colors.yellow[200]!,
+    ]; */
+
+    return Container(
+        width: double.infinity,
+        height: 200,
+        child: PieChart(
+          dataMap: dataMap,
+          animationDuration: Duration(milliseconds: 800),
+          chartLegendSpacing: 32,
+          chartRadius: MediaQuery.of(context).size.width / 3.2,
+          // colorList: colorList,
+          initialAngleInDegree: 0,
+          chartType: ChartType.ring,
+          ringStrokeWidth: 32,
+          centerText: "HYBRID",
+          legendOptions: LegendOptions(
+            showLegendsInRow: false,
+            legendPosition: LegendPosition.right,
+            showLegends: true,
+            // legendShape: _BoxShape.circle,
+            legendTextStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          chartValuesOptions: ChartValuesOptions(
+            showChartValueBackground: true,
+            showChartValues: true,
+            showChartValuesInPercentage: false,
+            showChartValuesOutside: false,
+            decimalPlaces: 1,
+          ),
+        ));
   }
 }
